c9108c8c30b3012ff601f8add14c6988a80f2389
vk9759585-stack <vk9759585@gmail.com>
2026-08-02 22:56:32 +0530

    Enable post, reel, and story media uploads

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -1,27 +1,28 @@
-exports.uploadStory = async (req, res) => {
+const Story = require("../models/Story");
 
+exports.getStories = async (req, res) => {
     try {
-
-        const story = await Story.create({
-
-            user: req.user.id,
-
-            media: req.file.filename,
-
-            isVideo: false
-
-        });
-
-        res.status(201).json(story);
-
+        const stories = await Story.find({ expiresAt: { $gt: new Date() } })
+            .populate("user", "name username profileImage verified")
+            .sort({ createdAt: -1 });
+        res.json({ success: true, data: stories });
     } catch (err) {
-
-        res.status(500).json({
-
-            message: err.message
-
-        });
-
+        res.status(500).json({ success: false, message: err.message });
     }
+};
 
+exports.uploadStory = async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({ message: "Story media is required" });
+        }
+        const story = await Story.create({
+            user: req.user.id,
+            media: req.file.filename,
+            isVideo: req.file.mimetype.startsWith("video/")
+        });
+        res.status(201).json(story);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 };
