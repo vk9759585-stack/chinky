7dc0f8f2550ad7e45d58920369118a8f54f3cc12
vk9759585-stack <vk9759585@gmail.com>
2026-08-02 22:49:09 +0530

    Add profile update route

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -5,5 +5,6 @@ const auth = require("../middleware/authMiddleware");
 const controller = require("../controllers/profileController");
 
 router.get("/", auth, controller.getProfile);
+router.put("/", auth, controller.updateProfile);
 
 module.exports = router;
